b2bf0263824385dfc12f809387c82a0332f36ac1
749032 <naazbhagat1712@gmail.com>
2024-03-24 22:43:46 +0530

    Commit successfull

---
+++ Assignments/Asg1_Ch10/script.js
@@ -0,0 +1,7 @@
+alert("HI");
+let country="India";
+let continent="Asia";
+let population = '1,428,627,663';
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ coursematerial/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,15 @@
+
+let js = 'amazing';
+if (js === 'amazing') alert('Javascript is Fun');
+
+48 + 23 + 23 - 10;
+console.log(48 + 23 + 23 - 10)
+
+console.log('Jonas');
+console.log(22);
+
+let firstname = 'Matrix';
+console.log(firstname);
+console.log(firstname);
+console.log(firstname);
+console.log(firstname);
